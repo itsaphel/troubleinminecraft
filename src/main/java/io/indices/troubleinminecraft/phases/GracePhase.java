6d4734207d59cd9158ffe9a981314812b3f9acd8
aphel <aphel@indices.io>
2017-07-13 09:08:02 +0100

    nothing to see here

--- src/main/java/io/indices/troubleinminecraft/phases/GracePhase.java
+++ src/main/java/io/indices/troubleinminecraft/phases/GracePhase.java
@@ -55,6 +55,6 @@ public class GracePhase extends TimedPhase {
 
         PreGameFeature preGameFeature = getGame().createFeature(PreGameFeature.class, this);
         preGameFeature.setScoreboard(scoreboardFeature.getScoreboard());
-        addFeature(scoreboardFeature);
+        addFeature(preGameFeature);
     }
 }
