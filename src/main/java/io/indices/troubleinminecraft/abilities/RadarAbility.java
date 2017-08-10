b6195efa5e51c4bf17ddf8f62626ffca28bab0b0
aphel <aphel@indices.io>
2017-08-10 01:24:27 +0100

    actually make the compass work. yes, that's right, it works!!

--- src/main/java/io/indices/troubleinminecraft/abilities/RadarAbility.java
+++ src/main/java/io/indices/troubleinminecraft/abilities/RadarAbility.java
@@ -10,6 +10,7 @@ import com.voxelgameslib.voxelgameslib.utils.ItemBuilder;
 import net.kyori.text.LegacyComponent;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -49,7 +50,7 @@ public class RadarAbility extends Ability {
         if (games.size() == 1) {
             Game game = games.get(0);
 
-            User nearest = getNearestPlayer(game.getPlayers());
+            User nearest = getNearestPlayer(game.getPlayers().stream().filter(user -> !user.getUuid().equals(affected.getUuid())).collect(Collectors.toList()));
 
             if (nearest != null) {
                 affected.getPlayer().setCompassTarget(nearest.getPlayer().getLocation());
@@ -65,7 +66,7 @@ public class RadarAbility extends Ability {
         for (User target : targets) {
             double distance = target.getPlayer().getLocation().distanceSquared(affected.getPlayer().getLocation());
 
-            if (distance < nearestDistance) {
+            if (nearest == null || distance < nearestDistance) {
                 nearestDistance = distance;
                 nearest = target;
             }
