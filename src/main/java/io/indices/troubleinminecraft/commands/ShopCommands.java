9c7fe00916e87f6ae29311a76e2baf9b9d253f53
aphel <aphel@indices.io>
2017-08-14 21:15:27 +0100

    open point shop

--- src/main/java/io/indices/troubleinminecraft/commands/ShopCommands.java
+++ src/main/java/io/indices/troubleinminecraft/commands/ShopCommands.java
@@ -1,27 +1,23 @@
 package io.indices.troubleinminecraft.commands;
 
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
 import com.google.inject.Injector;
 import com.voxelgameslib.voxelgameslib.components.inventory.InventoryHandler;
 import com.voxelgameslib.voxelgameslib.game.Game;
 import com.voxelgameslib.voxelgameslib.game.GameHandler;
 import com.voxelgameslib.voxelgameslib.user.User;
-
-import java.util.List;
-import javax.annotation.Nonnull;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-
-import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Default;
 import io.indices.troubleinminecraft.game.TIMData;
 import io.indices.troubleinminecraft.shop.Shop;
 import io.indices.troubleinminecraft.shop.ShopRegistry;
 
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.List;
+
 @Singleton
-@CommandAlias("shop")
 public class ShopCommands extends BaseCommand {
     @Inject
     private Injector injector;
@@ -30,9 +26,9 @@ public class ShopCommands extends BaseCommand {
     @Inject
     private InventoryHandler inventoryHandler;
 
-    @Default
+    @CommandAlias("shop|s")
     @CommandPermission("%user")
-    public void openShop(@Nonnull User sender) {
+    public void openRoleShop(@Nonnull User sender) {
         List<Game> games = gameHandler.getGames(sender.getUuid(), false);
 
         if (games.size() == 1) {
@@ -48,9 +44,7 @@ public class ShopCommands extends BaseCommand {
 
                 if (isTraitor || isDetective) {
                     ShopRegistry shopRegistry = timData.getShopRegistry();
-
                     Shop shop;
-
                     if (isTraitor) {
                         shop = shopRegistry.getTraitorShop();
                     } else {
@@ -59,6 +53,23 @@ public class ShopCommands extends BaseCommand {
 
                     sender.getPlayer().openInventory(shop.make(sender).getBukkitInventory());
                 }
+            });
+        }
+    }
+
+    @CommandAlias("pointshop|ps")
+    @CommandPermission("%user%")
+    public void openPointShop(@Nonnull User sender) {
+        List<Game> games = gameHandler.getGames(sender.getUuid(), false);
+
+        if (games.size() == 1) {
+            Game game = games.get(0);
+
+            game.getGameData(TIMData.class).ifPresent(timData -> {
+                ShopRegistry shopRegistry = timData.getShopRegistry();
+                Shop shop = shopRegistry.getPointsShop();
+
+                sender.getPlayer().openInventory(shop.make(sender).getBukkitInventory());
             });
         }
     }
